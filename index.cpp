1cc84b7d3b05161a17c801710d6f43988f27227b
ButterPeanuts <r116330@g.kobe-kosen.ac.jp>
2023-04-19 11:29:30 +0900

    Debug, Releaseのビルドを試す

--- index.cpp
+++ index.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+
+//BUILD_TYPEについて
+#if !defined(BUILD_TYPE)
+	#define BUILD_TYPE "<None>"
+#endif
+
 int main(){
 	std::cout << "Hello, World" << std::endl;
+	std::cout << "BUILD_TYPE=" BUILD_TYPE << std::endl;
 }
